d9b8451079606b4a218ad0b5a70fedf9dff2c230
normainobary2021 <norman.ainobushoborozi@gmail.com>
2022-11-15 22:04:11 +0300

    Add header functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './Navigation/Header';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
